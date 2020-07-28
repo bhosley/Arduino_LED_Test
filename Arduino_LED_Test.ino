80176e841267c1b03541318a4f3994cf8f25975d
bhosley <brandonscotthosley@gmail.com>
2020-07-28 14:14:28 -0600

    Syntax Update
    Improving syntax of the sketch.

--- Arduino_LED_Test.ino
+++ Arduino_LED_Test.ino
@@ -1,30 +1,35 @@
 #include <FastLED.h>
 #define PIN 4
-#define NUM_LEDS 27
+#define NUM_LEDS 27           //Longest chain of LEDs, or length to be tested
 
 CRGB leds[NUM_LEDS];
 
 int i = 0;
 
 void setup() {
-FastLED.addLeds<NEOPIXEL, PIN>(leds, NUM_LEDS);
-FastLED.setBrightness( 250);
+      FastLED.addLeds<NEOPIXEL, PIN>(leds, NUM_LEDS);
+      FastLED.setBrightness( 250);
 }
 
 void loop() { 
-if (i == NUM_LEDS +1 ) { i = 0; } else { //pulls dot back to beginning
-for(int i = 0; i < NUM_LEDS +1; i++) {  //positions dot, advances dot
-      leds[i].setRGB( 250 , 250 , 250 ); //sets dot to white
-      FastLED.show(); //send 
-      delay(500);
-      leds[i].fadeToBlackBy( 128 ); //flickers the dot
-      FastLED.show();
-      delay(100); 
-      leds[i].setRGB ( 0, 0, 0 ); //blacks out current dot position
-      FastLED.show();
-      delay(250);
-}
-} 
+      if( i == NUM_LEDS + 1 ) { 
+            i = 0;                                    //pulls dot back to beginning
+      } else { 
+            for( int i = 0; i < NUM_LEDS + 1; i++ ) { //sends a dot along the array of LEDs by int Addr
+                  //set LED i to white; show and wait
+                  leds[i].setRGB( 250 , 250 , 250 );  
+                  FastLED.show();                     
+                  delay( 500 );
+                  //
+                  leds[i].fadeToBlackBy( 128 );       //flickers the dot
+                  FastLED.show();
+                  delay( 100 ); 
+                  //
+                  leds[i].setRGB ( 0, 0, 0 );         //blacks out current dot position
+                  FastLED.show();
+                  delay( 250 );
+            }
+      } 
 }
 
 
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Arduino_LED_Test
+
+Simple sketch for testing arrays of addressable leds for integrity and QA for the LEDs themselves.
+
+Function is only to sequentially light the LEDs one at a time, in order until it reaches the end of the chain and starts over again. Like a dot moving along a line.
+
+It is possible to have forks in the chain of LEDs. In such a case the length of the largest chain should be appropriately declared. Corresponding nodes in different chains will light up as they share an address.
