a876033881c01632dc0db473af48532a45613c60
bhosley <brandonscotthosley@gmail.com>
2020-07-28 14:19:51 -0600

    Improve Readability
    Restructured some  of the code to improve readability. Changed comments for greater clarity

--- Arduino_LED_Test.ino
+++ Arduino_LED_Test.ino
@@ -1,6 +1,6 @@
 #include <FastLED.h>
 #define PIN 4
-#define NUM_LEDS 27           //Longest chain of LEDs, or length to be tested
+#define NUM_LEDS 27     //Longest chain of LEDs, or length to be tested
 
 CRGB leds[NUM_LEDS];
 
@@ -12,20 +12,24 @@ void setup() {
 }
 
 void loop() { 
+      // Check for index out of bounds
       if( i == NUM_LEDS + 1 ) { 
-            i = 0;                                    //pulls dot back to beginning
+            i = 0;
       } else { 
-            for( int i = 0; i < NUM_LEDS + 1; i++ ) { //sends a dot along the array of LEDs by int Addr
-                  //set LED i to white; show and wait
+            for( int i = 0; i < NUM_LEDS + 1; i++ ) {
+                  
+                  //set LED i to white; send command and wait
                   leds[i].setRGB( 250 , 250 , 250 );  
                   FastLED.show();                     
                   delay( 500 );
-                  //
-                  leds[i].fadeToBlackBy( 128 );       //flickers the dot
+
+                  // Slowly fade LED i to black; send command and wait
+                  leds[i].fadeToBlackBy( 128 );       
                   FastLED.show();
                   delay( 100 ); 
-                  //
-                  leds[i].setRGB ( 0, 0, 0 );         //blacks out current dot position
+
+                  // Turn off LED i; send command and wait
+                  leds[i].setRGB ( 0, 0, 0 );
                   FastLED.show();
                   delay( 250 );
             }
